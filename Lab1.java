d7ebfa7c25855bae9900b4d477417df19a3abc3a
Timur <35903356+tplaymeow@users.noreply.github.com>
2020-10-11 04:45:41 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -1,41 +1,39 @@
 import static java.lang.Math.*;
 
 class Lab1 {
-	public static void main(String args[]) {
-		int[] d = {6, 8, 10, 12, 14, 16, 18};
-		
-		double[] x = new double[19];
-		for(int i = 0; i < 19; i++) x[i] = -12 + random()*(11 + 12);
+        public static void main(String args[]) {
+                int[] g = {6, 8, 10, 12, 14, 16, 18};
 
-		double[][] matrix = new double[7][19];
+                double[] x = new double[19];
+                for(int i = 0; i < 19; i++) x[i] = -12 + random()*(11 + 12);
 
-		for(int i = 0; i < 7; i++){
-			for(int j = 0; j < 19; j++){
-				switch(d[i]){
+                double[][] b = new double[7][19];
 
-					case(12):
-						double num = (pow(tan(x[j]), 2) + 1) / (2*PI);
-						double exponent = pow(atan((x[j]-0.5)/23) / 2, exp(x[j]));
-						matrix[i][j] = log(pow(num, exponent));
-						break;
+                for(int i = 0; i < 7; i++){
+                        for(int j = 0; j < 19; j++){
+                                switch(g[i]){
 
-					case(8):
-						continue;
-					case(16):
-						continue;
-					case(18):
-						matrix[i][j] = sin(cos(log(abs(x[j]))));
-						break;
+                                        case(12):
+                                                double num = (pow(tan(x[j]), 2) + 1) / (2*PI);
+                                                double exponent = pow(atan((x[j]-0.5)/23) / 2, exp(x[j]));
+                                                b[i][j] = log(pow(num, exponent));
+                                                break;
 
-					default:
-						matrix[i][j] = cbrt(cbrt(tan(atan((x[j]-0.5)/23))));
-						break;
-				}
-			}
-		}
-		for(double[] line : matrix){
-			for(double element : line) System.out.printf("%5.2f ", element);
-			System.out.println();
-		}
-	}
+                                        case(8):
+                                        case(16):
+                                        case(18):
+                                                b[i][j] = sin(cos(log(abs(x[j]))));
+                                                break;
+
+                                        default:
+                                                b[i][j] = cbrt(cbrt(tan(atan((x[j]-0.5)/23))));
+                                                break;
+                                }
+                        }
+                }
+                for(double[] line : b){
+                        for(double element : line) System.out.printf("%5.2f ", element);
+                        System.out.println();
+                }
+        }
 }
